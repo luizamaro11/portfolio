da7a592aa36f5d1a4895357ee4aa83d32f69de51
luizamaro11 <luiz.amaro@gmail.com>
2025-04-29 23:22:04 -0300

    Adicionando as brands das redes sociais no projeto

---
+++ server/config.example.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASE_URL', 'http://domain.com.br');
+
+define('EMAIL_HOST', '');
+define('EMAIL_USERNAME', '');
+define('EMAIL_PASSWORD', ''); // Use uma App Password se estiver usando Gmail
+define('EMAIL_FROM', '');
+define('EMAIL_FROM_NAME', '');
